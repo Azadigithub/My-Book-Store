f53e64c164d61adfd4593537ccce7e00236869a6
Ali <aliazadigithub@gmail.com>
2025-06-10 16:31:23 +0330

    Vaild login and signin

--- Scripts/Signin.js
+++ Scripts/Signin.js
@@ -7,18 +7,35 @@ let emailerr = document.getElementById("email-err");
 let login = document.getElementById("login");
 let successmessage = document.getElementById("success-message");
 let enteredpassword;
-
+let passwordvalid = false;
+let phonenumbervalid = false;
+// let passwordvalid = false;
 login.addEventListener("click", function (event) {
-  console.log("ssf");
-  event.preventDefault()
-  setTimeout(function () {
-    //   successmessage.style.display = "block";
-      successmessage.style.opacity = "1";
-      successmessage.style.visibility = "visible";
-      setTimeout(function () {
-        window.location.href = "../../index.html"; // یا آدرس صفحه‌ی اصلی
-      }, 3000);
-  }, 2000);
+  if(passwordvalid & phonenumbervalid){
+    event.preventDefault()
+    setTimeout(function () {
+      //   successmessage.style.display = "block";
+          successmessage.innerHTML= '  ✅ ورود با موفقیت انجام شد!'
+          successmessage.style.backgroundColor = "#89e8a0";
+        successmessage.style.opacity = "1";
+        successmessage.style.visibility = "visible";
+        setTimeout(function () {
+          window.location.href = "../../index.html"; // یا آدرس صفحه‌ی اصلی
+        }, 3000);
+    }, 2000);
+  }else{
+    event.preventDefault()
+    console.log('invalid');
+    successmessage.innerHTML= 'نام کاربری یا رمز عبور نامعتبر!'
+    successmessage.style.opacity = "1";
+    successmessage.style.backgroundColor = "#ef6767";
+    successmessage.style.visibility = "visible";
+    setTimeout(function(){
+      successmessage.style.opacity = "0";
+      successmessage.style.visibility = "hidden";
+    },3000)
+    
+  }
 });
 password.addEventListener("keyup", function (event) {
   console.log(event.target.value.length);
@@ -31,6 +48,7 @@ password.addEventListener("keyup", function (event) {
   } else {
     // passwordError.style.color = 'green';
     // passwordError.innerHTML = 'رمز عبور به درستی وارد شد';
+    passwordvalid=true;
     passwordError.style.opacity = "0";
   }
 });
@@ -43,17 +61,22 @@ mobilenumber.addEventListener("keyup", function (event) {
   if (event.target.value.length == 0) {
     mobileError.style.color = "red";
     mobileError.innerHTML = "وارد کردن شماره همراه الزامی است!";
+    phonenumbervalid=false;
   } else if (!regex.test(event.target.value)) {
     mobileError.style.color = "red";
     if (!event.target.value.startsWith("09")) {
       mobileError.innerHTML = "شماره همراه باید با 09 شروع شود!";
+      phonenumbervalid=false;
     } else if (!/^\d+$/.test(event.target.value)) {
       mobileError.innerHTML = "شماره همراه فقط باید شامل اعداد باشد!";
+      phonenumbervalid=false;
     } else if (event.target.value.length !== 11) {
       mobileError.innerHTML = "شماره همراه باید 11 رقم باشد!";
+      phonenumbervalid=false;
     }
   } else {
     mobileError.style.opacity = "0";
+    phonenumbervalid=true;
   }
 });
 email.addEventListener("keyup", function (event) {
